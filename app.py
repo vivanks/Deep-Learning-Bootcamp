7ac461e39f9daef38473f6513c9c56e640f8927f
Vivank Sharma <vivanksharma@ymail.com>
2020-04-20 23:40:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,1 @@
-import os
-from flask import Flask,redirect
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    print("Hello")
-    return redirect("https://ds.archive.codes", code=302)
-
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+print("hello")
